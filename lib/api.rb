d727462fdff225bd55e009310c2096ea53835061
Christopher Diercksen <christopher.diercksen@gmail.com>
2021-03-20 17:58:30 -0400

    separated load_characters method from load_location method

--- lib/api.rb
+++ lib/api.rb
@@ -1,29 +1,33 @@
 class API
-    def self.load_location_characters
+    def self.load_location
         resp_location = RestClient.get("https://rickandmortyapi.com/api/location")
         location_hash  = JSON.parse(resp_location.body, symbolize_names: true)
         location_array = location_hash[:results]
 
-        resp_character = RestClient.get("https://rickandmortyapi.com/api/character")
-        character_hash = JSON.parse(resp_character.body, symbolize_names: true)
-        character_array = character_hash[:results]
-
+        
         location = location_array.collect do | locale |
-        # name = locale[:name]
-        # url = locale[:url]
-        # residents = locale[:residents]
-        Location.new(locale)
-        # binding.pry
+            # name = locale[:name]
+            # url = locale[:url]
+            # residents = locale[:residents]
+            Location.new(locale)
+            # binding.pry
         end
-
-        character = character_array.collect do | char |
+        
+    end
+    
+    def self.load_characters(url)
+        resp_character = RestClient.get(url)
+        character_hash = JSON.parse(resp_character.body, symbolize_names: true)
+        character_array = character_hash[:name]
+    binding.pry
+     character = character_array.collect do | char |
         # name = locale[:name]
         # url = locale[:url]
         # residents = locale[:residents]
         Character.new(char)
         end
     end
-     
+
     def self.load_location_details(new_location)
         resp = RestClient.get(new_location.url)
         new_location_hash = JSON.parse(resp.body, symbolize_names: true)
